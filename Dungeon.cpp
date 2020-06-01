096727915757101d32617f51f2c60f3f010709d3
futurepr0n <mark@futurepr0n.com>
2020-06-01 12:59:34 -0400

    new code changes to handle first input

--- Dungeon.cpp
+++ Dungeon.cpp
@@ -10,13 +10,36 @@ Dungeon::Dungeon(Player p){
 
 }
 
+void Dungeon::printActions(int numActions, string actions[]){
+    cout << "Choose an action: \n";
+    for (int i = 0; i < numActions; i++){
+        cout << actions[i] << "\n";
+    }
+}
+
+void Dungeon::handleEmptyRoom(Room *room){
+    cout << "You enter the room but it is empty.\n";
+    string actions[] = {"A. Move to another room"};
+    while(true){
+        printActions(1, actions);
+        string input;
+        cin >> input;
+        if (input== "A" || input == "a"){
+            return;
+        }else{
+            cout << "Incorrect choice\n";
+        }
+        
+    }
+}
+
 void Dungeon::enterRoom(Room *room){
     if (room->enemies.size() !=0){
         //handle room with enemy
     }else if (room ->items.size()!=0){
         //handle room with chest
     } else {
-        
+
     }
 
 }
